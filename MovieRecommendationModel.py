ffc9ea1df225f0a128d3a19c5d0d30bb3d08e996
boson-gamma-kplr elie <boson.gamma@training.kplr.fr>
2023-09-13 13:49:22 +0000

    fix model saving

--- MovieRecommendationModel.py
+++ MovieRecommendationModel.py
@@ -7,7 +7,7 @@ class MovieRecommendationModel():
         """ init """
         self.spark_ = spark
 
-    def trainModel(self, movie_ratings, maxIter, rank, regParam, seed = 42):
+    def trainModel(self, movie_ratings, maxIter, rank, regParam, modelName = "model", seed = 42):
         """Build the recommendation model using ALS on the training data"""
 
         (training, test) = movie_ratings.randomSplit([.8, .2], seed)
@@ -17,6 +17,8 @@ class MovieRecommendationModel():
 
         # fit the ALS model to the training set
         self.model_=als.fit(training)
+
+        self.model_.write().overwrite().save("./Models/"+modelName)
 
         predictions = self.model_.transform(test)
         evaluator = RegressionEvaluator(metricName='rmse', labelCol='rating', predictionCol='prediction')
@@ -30,13 +32,11 @@ class MovieRecommendationModel():
         als_model = ALS(userCol='userID', itemCol='movieId', ratingCol='rating', coldStartStrategy='drop')
 
         # create the parameter grid
-        params = ParamGridBuilder().addGrid(als_model.regParam, [.01, .05, .1, .15]).addGrid(als_model.rank, [10, 50, 100, 150]).build()
+        params = ParamGridBuilder().addGrid(als_model.regParam, [.01, .05]).addGrid(als_model.rank, [10,30,50]).build()
 
         evaluator = RegressionEvaluator(metricName='rmse', labelCol='rating', predictionCol='prediction')
 
         #instantiating crossvalidator estimator
         cv = CrossValidator(estimator=als_model, estimatorParamMaps=params, evaluator=evaluator, parallelism=4)
-        best_model = cv.fit(movie_ratings)
-        model = best_model.bestModel
-
-        model.save("Model")
+        self.model_ = cv.fit(movie_ratings)
+        self.model_.bestModel.write().overwrite().save("./Models/model_opti")
